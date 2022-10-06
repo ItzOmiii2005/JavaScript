aa409ed98bd7af1259da259a3ecf3861debd0d24
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2022-10-06 03:02:46 -0700

    thx om

--- JavaScript/script1.js
+++ JavaScript/script1.js
@@ -1,4 +1,16 @@
+console.time("This Code!");
 console.log("Script is running...");
 console.log(40);
 console.log("Harsh Kale \n");
 console.log(20+20);
+console.log([1,2,34,5]);
+console.log({harsh: 'this', marks: 70});
+console.log(true);
+console.table({Harsh: 'me', marks: 100, Omanand: 'this', marks: 100, Akshay: 'friend', marks: 10, Prathmesh: 'close', marks: 90});
+console.log(false);
+console.error("This is Self Generated Error by ~ Harsh Kale");
+console.warn("This is warning in the console by Harsh Kale!");
+console.assert(20>87, "This is false!");
+console.timeEnd("This Code!");
+// console.clear();
+// console.count(); //shows how many default states are there.
