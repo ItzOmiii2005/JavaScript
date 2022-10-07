64b3ac1640c15646b6597fd13f70081a20e9e4c8
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2022-10-07 00:23:54 -0700

    All Ohk!

---
+++ JavaScript/script2.js
@@ -0,0 +1,17 @@
+console.log("Script 2 is running....");
+
+var name = "Harsh";
+console.log(name);
+
+console.log("Hii \n My Name is " + name);
+
+let village = "Latur";
+{
+    let village = "Almala";
+    village = "Ausa";
+    console.log(village);
+}
+console.log(village);
+
+arr = [12,34,45,56,76,7,9];
+console.log(arr);
